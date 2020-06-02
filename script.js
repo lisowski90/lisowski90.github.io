14c6d17de9ecd44fac967531781fda2005791f7b
lisowski90 <lisowski1990@gmail.com>
2020-06-02 08:21:00 +0200

    v0.2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'http://pacific-reef-37638.herokuapp.com/v1/task/';
+  var apiRoot = 'https://pacific-reef-37638.herokuapp.com/v1/task/';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
