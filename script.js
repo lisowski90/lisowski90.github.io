4c45c17158e9d32f4c81ae2fdd014b0a322e592e
lisowski90 <lisowski1990@gmail.com>
2020-06-18 13:38:28 +0200

    slash delete

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'https://morning-depths-95847.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'https://morning-depths-95847.herokuapp.com/v1/trello/';
+  const apiRoot = 'https://morning-depths-95847.herokuapp.com/v1/task';
+  const trelloApiRoot = 'https://morning-depths-95847.herokuapp.com/v1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
